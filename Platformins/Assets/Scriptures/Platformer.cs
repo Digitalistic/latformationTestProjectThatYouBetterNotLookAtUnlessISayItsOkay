ae84a8a9f1c0c52e1b07a81b9651ee83f39492e6
Ryan Schaefer <RyanLSchaefer@gmail.com>
2014-02-17 21:19:18 -0600

    Basic walk, run, and jump via Spelunky
    You can walk, run, and jump, all with physics carefully studied from
    Spelunky.

--- Platformins/Assets/Scriptures/Platformer.cs
+++ Platformins/Assets/Scriptures/Platformer.cs
@@ -3,15 +3,62 @@ using System.Collections;
 
 public class Platformer : MonoBehaviour 
 {
+	public float walkSpeed;
+	public float runSpeed;
+	public float jumpSpeed;
+	public float gravScale;
 
+	Vector2 myVelo;
+
+	float xIn, yIn;
+
+	bool isRunning = false;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		rigidbody2D.gravityScale = gravScale;	//Gravity, starring Sandra Bullock, coming soon to DVD and Blu-Ray!
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		ProcessMovement();
+	}
+
+	void ProcessMovement()
+	{
+		myVelo = rigidbody2D.velocity;	//get the current velocity. we're going to mess with this!
+		
+		xIn = Input.GetAxis("Horizontal");
+		yIn = Input.GetAxis("Vertical");
+		
+		if(isRunning)	//if we're running, use run speed, and check for button release
+		{
+			myVelo.x = xIn * runSpeed;
+
+			if(Input.GetButtonUp("Run"))
+				isRunning = false;
+		}
+		else   //if we're walking, use walk speed and check for run button press
+		{
+			myVelo.x = xIn * walkSpeed;
+			
+			if(Input.GetButtonDown("Run"))
+				isRunning = true;
+		}
+		
+		if(Input.GetButtonDown("Jump"))	//jumpin
+		{
+			myVelo.y = jumpSpeed;
+		}
+
+
+		if(Mathf.Abs(myVelo.y) > jumpSpeed)	//limit y-axis speed for constant falling
+		{
+
+		}
+
+		rigidbody2D.velocity = myVelo;	//apply our given velocity
 	}
 }
