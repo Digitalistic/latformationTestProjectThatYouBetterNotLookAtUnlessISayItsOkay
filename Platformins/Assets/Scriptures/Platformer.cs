e07c9d0d32b871d384d8fd56a152794120d51c4b
Ryan Schaefer <RyanLSchaefer@gmail.com>
2014-02-17 22:02:45 -0600

    Ground checks for jumping
    There’s now a floor check done via trigger so that you can’t jump again
    when in mid-air. Problems notices: It’s hard as fuck to get a good
    running jump off a platform edge. This is due to the interpolation.
    Turn it off and it’s easy, but then movement looks fucking awful. Also
    problematic is the 3 blocks to the player’s left. They’re 3 separate
    blocks of identical size at an identical height, yet somehow, one of
    them occasionally collides with the player. This should not be
    possible, but somehow it’s happening, and it’s fucking annoying.

--- Platformins/Assets/Scriptures/Platformer.cs
+++ Platformins/Assets/Scriptures/Platformer.cs
@@ -13,6 +13,7 @@ public class Platformer : MonoBehaviour
 	float xIn, yIn;
 
 	bool isRunning = false;
+	bool isGrounded = false;
 
 	// Use this for initialization
 	void Start () 
@@ -32,33 +33,70 @@ public class Platformer : MonoBehaviour
 		
 		xIn = Input.GetAxis("Horizontal");
 		yIn = Input.GetAxis("Vertical");
+
+		yIn = yIn + 0;	//This line only exists so I can have 0 warnings.
 		
 		if(isRunning)	//if we're running, use run speed, and check for button release
 		{
 			myVelo.x = xIn * runSpeed;
 
 			if(Input.GetButtonUp("Run"))
-				isRunning = false;
+				EndRunning();
 		}
 		else   //if we're walking, use walk speed and check for run button press
 		{
 			myVelo.x = xIn * walkSpeed;
 			
 			if(Input.GetButtonDown("Run"))
-				isRunning = true;
+				BeginRunning();
 		}
-		
-		if(Input.GetButtonDown("Jump"))	//jumpin
+
+		if(isGrounded)
 		{
-			myVelo.y = jumpSpeed;
+			if(Input.GetButtonDown("Jump"))
+			{
+				Jump();
+			}
 		}
 
 
 		if(Mathf.Abs(myVelo.y) > jumpSpeed)	//limit y-axis speed for constant falling
 		{
-
+			if(myVelo.y > 0)
+				myVelo.y = jumpSpeed;
+			else
+				myVelo.y = -jumpSpeed;
 		}
 
 		rigidbody2D.velocity = myVelo;	//apply our given velocity
+	}
+
+	void Jump()	//Jump around. Jump around. Jump up, jump up and get down!
+	{
+		myVelo.y = jumpSpeed;
+	}
+
+	//These two are seperate functions in case you want something to happen like a speed effect or whatever
+	void BeginRunning()
+	{
+		isRunning = true;
+	}
+
+	void EndRunning()
+	{
+		isRunning = false;
+	}
+
+	//These two are seperate functions in case you want something to happen like dust particles or whatever
+	public void LandOnFloor()
+	{
+		Debug.Log("On Floor");
+		isGrounded = true;
+	}
+
+	public void LeaveFloor()
+	{
+		Debug.Log("Floor Departed");
+		isGrounded = false;
 	}
 }
